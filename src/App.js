f6cfcbbc511bf81400b3bce437c9da37b291d00e
Angry-Creator <nwankwochidera252565@gmail.com>
2022-11-18 15:36:25 +0000

    Starting the blog

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import "./index.css";
-import WelcomePage from "./WelcomePage";
+
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/welcomepage" element={<WelcomePage />} />
-        </Routes>
-      </Router>
-    </div>
+    <div>Hello World</div>
   )
 }
 
